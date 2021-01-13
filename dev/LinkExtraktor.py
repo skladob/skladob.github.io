2681ca3d730797f6c00f29d0780f1255e2da6cd0
skladob <skladob@hde.msk.ru>
2021-01-14 02:27:04 +0300

    Initiate comit

--- dev/LinkExtraktor.py
+++ dev/LinkExtraktor.py
@@ -1,21 +1,45 @@
+import re
+import sqlite3
+
 import link_extractor
 
-
-def get_link(url):
-    links = link_extractor.getLinks(url)
-    for link in links:
-        result = link_extractor.getLinks(link)
-        for a in result:
-            print(a)
+SQL_file = 'dev/newDataBase.db'
+QUERY_reader_host = 'SELECT host FROM Manufacturers'
 
 
+def create_connection(connection):
+    connect = None
+    try:
+        with sqlite3.connect(connection) as connect:
+            cursor = connect.cursor()
+            print("Установлно соединение с SQLite")
+            return cursor
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+
+
+def reading_from_base(connection, query):
+    cursor = create_connection(connection)
+    cursor.execute(query)
+    for result in cursor:
+        print(result)
 
 
 
 
 
 
+
+
+    # for row in cursor.fetchone():
+    #     # return row
+    #     links = link_extractor.getLinks(row)
+    #     for link in links:
+    #         result = link_extractor.getLinks(link)
+    #         for a in result:
+    #             print(a)
 
 
 if __name__ == "__main__":
-    get_link('https://sivik.ru')
+    reading_from_base(SQL_file, QUERY_reader_host)
+    # print (row)
