1a0feb9c694062c648ca18384cbe89b4eada29d8
skladob <skladob@hde.msk.ru>
2021-01-17 01:19:14 +0300

    commit

--- dev/create_db.py
+++ dev/create_db.py
@@ -3,20 +3,11 @@
 
 import sqlite3
 
-import link_extractor
 
-url = 'https://sivik.ru'
-query = 'INSERT INTO href VALUES (site)'
-
-links = link_extractor.getLinks(url)
 connect = sqlite3.connect('newDataBase.db')
 connect.row_factory
 cursor = connect.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS href(site TEXT)""")
-connect.commit()
-for data_links in links:
-    cursor.execute(query, data_links)
-
 connect.commit()
 cursor.close()
 connect.close()
