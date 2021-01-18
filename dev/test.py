97668c0516bd40891e8addd602a6008325ba829d
skladob <skladob@hde.msk.ru>
2021-01-18 14:07:39 +0300

    Initiate commit

--- dev/test.py
+++ dev/test.py
@@ -5,17 +5,20 @@ import sqlite3
 
 import link_extractor
 
-url = 'https://sivik.ru'
-query = 'INSERT INTO href VALUES (site)'
+URL = 'https://sivik.ru'
+QUERY = 'INSERT INTO href VALUES (?)'
+DATABASE = r'SQL/db.sqlite3'
 
-links = link_extractor.getLinks(url)
-connect = sqlite3.connect('newDataBase.db')
+links = link_extractor.getLinks(URL)
+connect = sqlite3.connect(DATABASE)
 connect.row_factory
 cursor = connect.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS href(site TEXT)""")
 connect.commit()
 for data_links in links:
-    cursor.execute(query, data_links)
+# print(type(links))
+# print(links)
+    cursor.executemany(QUERY, links)
 
 connect.commit()
 cursor.close()
